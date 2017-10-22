a8713a754a0a5e2aaeb415af3d9b5d6b2ecb2716
Stivo32 <stivo32@gmail.com>
2017-10-22 11:02:44 -0700

    modified for gunicorn

--- etc/init.sh
+++ etc/init.sh
@@ -1,3 +1,5 @@
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
+cd ..
+gunicorn -c ./etc/hello.py hello:app
